3fcc587e8cd2e6f832adc25f11e0a63cb510cff4
Lilian Harvey <lharvey@student.hive.fi>
2020-02-07 17:49:56 +0200

    Main now gets Key Presses. Still messing w/ lib.

--- main.c
+++ main.c
@@ -12,6 +12,13 @@
 
 #include <unistd.h>
 #include "mlx.h"
+#include "libft/libft.h"
+
+int	key_press(int keycode)
+{
+	ft_putnbr(keycode);
+	return (0);
+}
 
 int main()
 {
@@ -25,7 +32,7 @@ int main()
 	int				colour;
 	unsigned int	x;
 	unsigned int	y;
-
+	
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Basic Program");
 	image = mlx_new_image(mlx_ptr, 500, 500);
@@ -36,13 +43,15 @@ int main()
 	buffer_32bit = (int *)mlx_get_data_addr(image, &pixel_bits, &line_bytes, &endian);
 	line_bytes /= 4;
 	
-	for(y = 0; y < 250; ++y)
-	for(x = 0; x < 250; ++x)
+	while (y >= 0 && y <= 250 && x >= 0 && x <= 250)
 	{
     	buffer_32bit[(y * line_bytes) + x] = colour;
+		++x;
+		++y;
 	}
 	mlx_put_image_to_window(mlx_ptr, win_ptr, image, 0, 0);
 //	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
 //	mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_hook(win_ptr, 2, 0, key_press, mlx_ptr);
 	mlx_loop(mlx_ptr); /* asks OS to do thing. This function also does the event management. */
 }
